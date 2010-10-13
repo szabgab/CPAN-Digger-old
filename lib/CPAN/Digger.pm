b0db32a1e1d677e43e76d9a95e1318febc09d1c0
Gabor Szabo <szabgab@gmail.com>
2010-10-13 12:03:37 +0200

    more database updating

--- lib/CPAN/Digger.pm
+++ lib/CPAN/Digger.pm
@@ -10,6 +10,7 @@ use File::Copy     qw(copy);
 use File::Path     qw(mkpath);
 use File::Spec;
 use Parse::CPAN::Packages;
+use YAML::Any      ();
 
 use CPAN::Digger::DB;
 
@@ -17,31 +18,51 @@ has 'tt'     => (is => 'ro', isa => 'Str');
 has 'cpan'   => (is => 'ro', isa => 'Str');
 has 'output' => (is => 'ro', isa => 'Str');
 
+my %db;
+
 sub run_index {
 	my $self = shift;
 
 	my $p = Parse::CPAN::Packages->new( File::Spec->catfile( $self->cpan, 'modules', '02packages.details.txt.gz' ));
 
-	my %db =  CPAN::Digger::DB->dbh;
+	%db =  CPAN::Digger::DB->dbh;
 
 	my @distributions = $p->distributions;
 	foreach my $d (@distributions) {
-		say $d->prefix;
+		LOG("Working on " . $d->prefix);
 		my $path = dirname $d->prefix;
 		my $src  = File::Spec->catfile( $self->cpan, 'authors', 'id', $d->prefix );
-		my $dir = File::Spec->catdir( $self->output, 'id' , lc $d->cpanid);
+		my $dir  = File::Spec->catdir( $self->output, 'id' , lc $d->cpanid);
+
 		mkpath $dir;
 		chdir $dir;
-		if (substr($d->prefix, -7) eq '.tar.gz') {
-			my $cmd = "tar xzf $src";
-			#say $cmd;
-			my $out = qx{$cmd};
-			say '----';
-			say $out;
-			$db{distro}->update({ name => $d->dist }, { name => $d->dist, author => lc $d->cpanid } , { upsert => 1 })
-		} else {
-			warn "Skipping $src\n";
+		if (not -e File::Spec->catdir($dir, $d->distvname)) {
+			$self->unzip($d, $src);
 		}
+		if (not -e File::Spec->catdir($dir, $d->distvname)) {
+			WARN("No directory for $src");
+			next;
+			
+		}
+		
+		chdir $d->distvname;
+		my %data = (
+			name   => $d->dist,
+			author => lc $d->cpanid,
+		);
+
+		$data{has_meta} = -e 'META.yml';
+		# TODO we need to make sure the data we read from META.yml is correct and
+		# someone does not try to fill it with garbage or too much data.
+		if ($data{has_meta}) {
+			my $meta = YAML::Any::LoadFile('META.yml');
+			#print Dumper $meta;
+			$data{meta}{license} = $meta->{license};
+		}
+
+		LOG("Update DB");
+		$db{distro}->update({ name => $d->dist }, \%data , { upsert => 1 });
+		
 last if $main::counter++ > 5;
 	}
 
@@ -71,6 +92,26 @@ sub run {
 	print "\n</pre>\n";
 }
 
+sub WARN {
+	LOG("WARN: $_[0]");
+}
+sub LOG {
+	my $msg = shift;
+	print "$msg\n";
+}
+
+sub unzip {
+	my ($self, $d, $src) = @_;
+	if (substr($d->prefix, -7) eq '.tar.gz') {
+		my $cmd = "tar xzf $src";
+		LOG($cmd);
+		my $out = qx{$cmd};
+		#say '----';
+		#say $out;
+	} else {
+		WARN("Does not know how unzip $src");
+	}
+}
 
 1;
 
