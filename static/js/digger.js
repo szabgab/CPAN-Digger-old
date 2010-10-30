b261c2f9076f6a46005a8bbf74bbf366d72a2568
Gabor Szabo <szabgab@gmail.com>
2010-10-31 00:25:46 +0200

    add jquery and jquery-ui and a sample on creating a dialog

---
+++ static/js/digger.js
@@ -0,0 +1,25 @@
+$(function(){
+    $("#dialog").dialog({
+					autoOpen: false,
+    buttons: {
+        'button 1': function() {
+alert(1);
+            // handle if button 1 is clicked
+        },
+        'button 2': function() {
+alert(2);
+            // handle if button 2 is clicked
+        },
+        'button 3': function() {
+alert(3);
+            // handle if button 3 is clicked
+        }
+    }
+});
+
+				$('#dialog_link').click(function(){
+					$('#dialog').dialog('open');
+					return false;
+				});
+});
+
