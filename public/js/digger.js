c1db08767b1935b308d703eef770061d7954f96f
Gabor Szabo <gabor@szabgab.com>
2010-11-04 13:10:21 +0200

    white spaces

--- public/js/digger.js
+++ public/js/digger.js
@@ -3,23 +3,24 @@ $(function(){
 					autoOpen: false,
     buttons: {
         'button 1': function() {
-alert(1);
-            // handle if button 1 is clicked
+                alert(1);
+                // handle if button 1 is clicked
         },
         'button 2': function() {
-alert(2);
-            // handle if button 2 is clicked
+                alert(2);
+                // handle if button 2 is clicked
         },
         'button 3': function() {
-alert(3);
-            // handle if button 3 is clicked
+                alert(3);
+                // handle if button 3 is clicked
         }
     }
-});
+    });
 
-				$('#dialog_link').click(function(){
-					$('#dialog').dialog('open');
-					return false;
-				});
+    $('#dialog_link').click(function(){
+	$('#dialog').dialog('open');
+	return false;
+    });
+
 });
 
