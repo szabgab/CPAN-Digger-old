a8a4b04dee522d90bda45b8ec440107c5c75cd08
Gabor Szabo <gabor@szabgab.com>
2010-11-04 10:44:44 +0200

    move the templates from tt to views and start using Template Toolkit in Dancer

--- lib/CPAN/Digger/WWW.pm
+++ lib/CPAN/Digger/WWW.pm
@@ -5,17 +5,25 @@ our $VERSION = '0.1';
 
 # for development:
 # on the real server the index file will be static
-if ($^O =~ m/win32/i) {
-    get '/' => sub {
-        #send_file(path config->{public}, 'index.html');
-        send_file 'index.html';
-        #template 'index';
-    };
-}
+# if ($^O =~ m/win32/i) {
+    # get '/' => sub {
+        # #send_file(path config->{public}, 'index.html');
+        # send_file 'index.html';
+        # #template 'index';
+    # };
+# }
 
-get '/demo' => sub {
-    template 'index';
+get '/' => sub {
+    template 'index', {
+        keywords => 'x,y',
+    };
 };
+foreach my $page (qw(news faq licenses)) {
+    get "/$page" => sub {
+        template $page;
+    }
+};
+
 
 get '/dancer' => sub {
     content_type 'text/plain';
