2839900f983673471f11ebbcdc9b733d1e28167e
Gabor Szabo <gabor@szabgab.com>
2010-11-04 09:58:07 +0200

    add some experimental code to the Dancer web code

--- lib/CPAN/Digger/WWW.pm
+++ lib/CPAN/Digger/WWW.pm
@@ -3,8 +3,23 @@ use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
-get '/' => sub {
+# for development:
+# on the real server the index file will be static
+if ($^O =~ m/win32/i) {
+    get '/' => sub {
+        #send_file(path config->{public}, 'index.html');
+        send_file 'index.html';
+        #template 'index';
+    };
+}
+
+get '/demo' => sub {
     template 'index';
+};
+
+get '/dancer' => sub {
+    content_type 'text/plain';
+    to_dumper config;
 };
 
 true;
