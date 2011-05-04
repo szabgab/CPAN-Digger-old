415d0573065df57d1c27f01cae423d67c40d00b7
Gabor Szabo <szabgab@gmail.com>
2011-05-04 08:03:56 -0700

    accept crazy version numbers as well

--- script/collect.pl
+++ script/collect.pl
@@ -30,13 +30,35 @@ my $files = File::Find::Rule
    ->start( "$opt{cpan}/authors/id" );
 
 while (my $file = $files->match) {
+    # Sample files:
     # F/FA/FAKE1/My-Package-1.02.tar.gz
-    #print "$file\n";
-
-    if ($file =~ m{^\w/\w\w/(\w+)/([\w-]*?)-([\d.]+)(\.tar\.gz)$} ) {
+    # Z/ZI/ZIGOROU/Module-Install-TestVars-0.01_02.tar.gz
+    # G/GR/GREENBEAN/Asterisk-AMI-v0.2.0.tar.gz
+    # Z/ZA/ZAG/Objects-Collection-029targz/Objects-Collection-0.29.tar.gz
+    my $PREFIX     = qr{\w/\w\w/(\w+)/};
+    my $SUBDIRS    = qr{(?:[\w/-]+/)};
+    my $PACKAGE    = qr{([\w-]*?)};
+    my $VERSION_NO = qr{[\d._]+};
+    my $CRAZY_VERSION_NO = qr {[\w.]+};
+    my $EXTENSION  = qr{(?:\.tar\.gz)};
+    if ($file =~ m{^$PREFIX           # P/PA/PAUSEID
+                   $SUBDIRS?          # optional garbage
+                   $PACKAGE
+                   -v?($VERSION_NO)      # version
+                   $EXTENSION
+                   $}x ) {
         #print "$1  - $2 - $3\n";
         $db->insert_distro($1, $2, $3, $file, (stat "$opt{cpan}/authors/id/$file")[9], time);
-    } else {
+
+    # K/KR/KRAKEN/Net-Telnet-Cisco-IOS-0.4beta.tar.gz
+    } elsif ($file =~ m{^$PREFIX           # P/PA/PAUSEID
+                   $SUBDIRS?          # optional garbage
+                   $PACKAGE
+                   -v?($CRAZY_VERSION_NO)      # version
+                   $EXTENSION
+                   $}x ) {
+        $db->insert_distro($1, $2, $3, $file, (stat "$opt{cpan}/authors/id/$file")[9], time);
+     } else {
         warn "ERROR - could not parse filename $file\n";
     }
 }
