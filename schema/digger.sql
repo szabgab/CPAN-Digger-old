d4ec1d9c893f486f108770da84de453d3b48fd47
Gabor Szabo <szabgab@gmail.com>
2011-05-03 23:50:48 -0700

    stop uniqueness requirement

--- schema/digger.sql
+++ schema/digger.sql
@@ -1,8 +1,8 @@
 CREATE TABLE distro (
     id       INTEGER PRIMARY KEY,
-    author   VARCHAR(50),
-    name     VARCHAR(255) UNIQUE NOT NULL,
-    version  VARCHAR(30),
+    author   VARCHAR(50) NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    version  VARCHAR(30) NOT NULL,
     path     VARCHAR(255) UNIQUE,
     file_timestamp DATE,
     added_timestamp DATE
