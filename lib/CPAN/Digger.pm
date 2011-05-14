7e32e42f86d9c66107e5a85977a3fe3bc3513eb5
Gabor Szabo <szabgab@gmail.com>
2011-05-14 12:46:38 +0300

    move a lot explanations to the FAQ

--- lib/CPAN/Digger.pm
+++ lib/CPAN/Digger.pm
@@ -70,17 +70,6 @@ Running CPAN-Digger-WWW.pl will launch a stand-alone web server.
 
 =head1 Indexing
 
-=head2 Perl Critic
-
-We are running L<Perl::Critic> using level 4 on modules and collect the data in 
-the critic.txt file of each distribution.
-
-=head2 Minimum Required perl version
-
-For each module we check the mimimum required version using L<Perl::MinimumVersion> of 
-modules and setting minimum version of package.
-We also collect the version markers in the version.txt file of each distribution.
-
 =head2 Word indexing (planned)
 
 For each distribution split up the name and each part of the name becomes a word
@@ -106,15 +95,6 @@ for each word include where it could be found
    cgi     distro               CGI-Application
    cgi     module               CGI::Simple
    cgi     module               CGI::Application
-
-=head2 Authors
-
-Parse the 00whois.xml file and update the 'author' table.
-
-Reuse the content of the author.json files.
-
-Fetch the avatars and display them.
-
 
 =head1 AUTHOR
 
